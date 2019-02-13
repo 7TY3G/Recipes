21eddbf43fb5da218d1da15808e2d7446ccdd714
Thomas Finch <thomasgfinch@gmail.com>
2019-02-13 18:18:45 +0000

    Remove code generated for angular project boiler plate

--- Recipes.Web/ClientApp/src/app/app.module.ts
+++ Recipes.Web/ClientApp/src/app/app.module.ts
@@ -7,8 +7,6 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { RecipesListComponent } from './recipes-list/reciples-list.component';
 import { AddRecipeComponent } from './add-recipe/add-recipe.component';
 
@@ -17,8 +15,6 @@ import { AddRecipeComponent } from './add-recipe/add-recipe.component';
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     RecipesListComponent,
     AddRecipeComponent
   ],
@@ -28,8 +24,6 @@ import { AddRecipeComponent } from './add-recipe/add-recipe.component';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
       { path: 'recipes-list', component: RecipesListComponent },
       { path: 'add-recipe', component: AddRecipeComponent }
     ])
