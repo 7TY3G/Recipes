1fa228fecedc940ecdd5985bdd49fa0dfc58446a
Thomas Finch <thomasgfinch@gmail.com>
2019-02-18 17:09:42 +0000

    Update edit recipe to use reactive forms.

--- Recipes.Web/ClientApp/src/app/edit-recipe/edit-recipe.component.ts
+++ Recipes.Web/ClientApp/src/app/edit-recipe/edit-recipe.component.ts
@@ -1,5 +1,6 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { Recipe } from '../shared/interface/recipe';
 
@@ -7,7 +8,8 @@ import { Recipe } from '../shared/interface/recipe';
   selector: 'app-edit-recipe',
   templateUrl: './edit-recipe.component.html'
 })
-export class EditRecipeComponent {
+export class EditRecipeComponent implements OnInit {
+  recipeForm: FormGroup;
   recipe: Recipe = {
     id: 0,
     name: '',
@@ -16,23 +18,50 @@ export class EditRecipeComponent {
   id: string;
   saved: boolean = false;
 
-  constructor(private router: Router, private currentRoute: ActivatedRoute, private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private currentRoute: ActivatedRoute,
+    private http: HttpClient,
+    @Inject('BASE_URL') private baseUrl: string) {
     this.currentRoute.params.subscribe((queryParams: ParamMap) => {
       this.http.get<Recipe>(this.baseUrl + 'api/Recipe/GetById', { params: { id: queryParams['id'] } }).subscribe(result => {
+        this.populateForm(result);
         this.recipe = result;
       }, error => console.error(error));
     });
   }
 
-  saveRecipe({ value, valid }: { value: Recipe, valid: boolean }) {
-    if (valid) {
+  ngOnInit() {
+    this.recipeForm = this.formBuilder.group({
+      name: '',
+      rating: ''
+    });
+  }
+
+  saveRecipe(): void {
+    if (this.recipeForm.valid) {
+      this.populateRecipeResult();
+
       this.http.put<Recipe>(this.baseUrl + 'api/Recipe/Update', this.recipe).subscribe(result => {
         this.saved = true;
       }, error => console.error(error));
     }
   }
 
-  backToRecipes() {
+  backToRecipes(): void {
     this.router.navigate(['recipes-list']);
+  }
+
+  populateForm(recipe: Recipe): void {
+    this.recipeForm.setValue({
+      name: recipe.name,
+      rating: recipe.rating
+    })
+  }
+
+  populateRecipeResult(): void {
+    this.recipe.name = this.recipeForm.get('name').value;
+    this.recipe.rating = this.recipeForm.get('rating').value;
   }
 }
